937028ebe13e72e3318edebb51f8ca55dfe2cca1
VasilevAlexandr97 <sashaorpix@gmail.com>
2022-05-28 06:47:36 +0500

    delete char field from user model

--- vk_bot/database/models/user.py
+++ vk_bot/database/models/user.py
@@ -7,7 +7,6 @@ class User(Model):
     vk_id = fields.BigIntField()
     first_name = fields.CharField(max_length=255)
     last_name = fields.CharField(max_length=255)
-    char = fields.CharField(max_length=100)
     created_at = fields.DatetimeField(auto_now_add=True)
 
     class Meta:
